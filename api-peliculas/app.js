9aed22a5a627995f7a04e3e163f2e063284e3874
luisferlie <luisferlie@gmail.com>
2024-01-16 21:46:24 +0100

    añadido todo-app

--- api-peliculas/app.js
+++ api-peliculas/app.js
@@ -21,8 +21,6 @@ const cargarPeliculas = async() => {
 	try {
 		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
 
-		
-	
 		console.log(respuesta);
 
 		// Si la respuesta es correcta
